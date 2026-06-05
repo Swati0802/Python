e1b410918eb1d3dab2382289ab8294de7b9da01c
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-05 15:56:33 +0530

    Add Hello.py with basic print statements and variables

---
+++ Practice/Hello.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print("My name is Swati.", "My age is 18.")
+print(23)
+print(2+2)
+
+name = "Swati"
+age = 25
+print(age)
+age2 = age  # value of right side it will go to left side  (Short cut ctrl +/)(MultiLine """ """)
+print(age2)
+
+print("My name is : ", name)
+print("My age is : ", age)
+print(type(name))
+print(type(age))
