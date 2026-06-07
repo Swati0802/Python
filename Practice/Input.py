cf7cfbe788615f996ab67e82a32fdc75d0d4e498
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-07 13:19:12 +0530

    Add Input.py for user input and type demonstration

---
+++ Practice/Input.py
@@ -0,0 +1,10 @@
+#input("Enter your name:")
+name = input("Enter your name:")
+
+print("Welcome", name)
+print(type(name), name)
+
+val = int(input("Enter some value :"))
+print(type(val))
+val = float(input("Enter some value :"))
+print(type(val))
