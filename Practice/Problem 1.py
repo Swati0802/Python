1701090f4c00c21e14c26b1452b6f50573e899d2
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-08 16:07:42 +0530

    Add Problem 1 with sum, average, and area calculations

---
+++ Practice/Problem 1.py
@@ -0,0 +1,18 @@
+# Problem 1
+a = int(input("Enter a value of a :"))
+b = int(input("Enter a value of b :"))
+
+sum = a + b
+print("a + b =", sum)
+print("avg =", (a + b)/2)
+
+#Problem 2
+l = int(input("Enter side of square :"))
+area = l * l
+print("Area of square :", area)
+
+# Problem 3
+a = int(input("Enter a value of a :"))
+b = int(input("Enter a value of b :"))
+
+print(a >= b)
