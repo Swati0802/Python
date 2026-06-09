60100097dcf95dcbbb762380330dfc529c876f7e
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-09 13:56:12 +0530

    Create String.py

---
+++ Practice/String.py
@@ -0,0 +1,10 @@
+str1 = "Hello"
+str2 = 'World\n'
+str3 = '''How are you?'''
+
+print(str1 + " "+ str2 + str3)
+
+final = str1 +" "+ str2 + str3
+print(len(final)) # space also include in string's length
+# \n = new line 
+# \t = tab
