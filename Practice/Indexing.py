e9db8c9b911512e1c2db46f9015f945005f6e3d6
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-10 13:53:43 +0530

    Create Indexing.py

---
+++ Practice/Indexing.py
@@ -0,0 +1,14 @@
+str = "apna college"
+ch = str[4]
+print(ch)
+
+# or
+print(str[4])
+
+# Slicing
+ # ending index is not including
+print(str[0:5]) # print(str[:5])  --> from starting
+print(str[5:]) # print(str[5:len(str)])  --> both for going to end of string
+
+# Negative Index
+print(str[-3:-1])
