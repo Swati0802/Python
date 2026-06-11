254dd6ac90057c132e32619caf925d0580c13409
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-11 11:40:26 +0530

    Create String Function.py

---
+++ Practice/String Function.py
@@ -0,0 +1,12 @@
+str = "Hello World"
+print(str.endswith("ld")) # returns true if string end with substr
+
+str = "hello World"
+print(str.capitalize())
+
+str = "Hello World"
+print(str.replace("o", "a")) #(old, new)
+
+print(str.find("o")) #where this letter or word, In output : give the index of this letter or word.
+
+print(str.count("o")) # how many times exist this word.
