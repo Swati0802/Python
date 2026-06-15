f758f6c09afc560e530671df37d6a1f8d037bd67
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-15 18:56:24 +0530

    Create List.py

---
+++ Practice/List.py
@@ -0,0 +1,12 @@
+# marks = [95, 91, 90, 89, 85]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(len(marks))
+
+# We can take any type of element in Python
+student = ["Karan", 85.5, "Delhi"]
+print(student[0])
+student[0] = "Arjun"
+print(student)
+# we cannot change in range/ length
