c3cf695ccc8faa072f96f01ac103f58e27fb08c8
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-19 22:43:25 +0530

    1

---
+++ Practice/Tuple.py
@@ -0,0 +1,20 @@
+# Tuple Slicing is also Possible
+tup = (2, 1, 3)
+print(tup)
+print(type(tup))
+print(tup[1])
+
+tup1 = () #empty tupple
+print(tup1)
+print(type(tup1))
+
+# RUght Tuple
+tup2 = (1,) # If single element. in Tupple, we must write as a(1,). # if Multiple. we havce a choice.
+print(tup2)
+print(type(tup2))
+
+
+#wrong Tuple
+tup2 = (1)
+print(tup2)
+print(type(tup2))
