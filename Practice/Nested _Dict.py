97ff9e04b6ebe348229b195d4c1c4ed0932c9fbc
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-26 00:11:25 +0530

    Create Nested _Dict.py

---
+++ Practice/Nested _Dict.py
@@ -0,0 +1,12 @@
+student = {
+    "name" : "shradha",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 99
+    }  
+}
+
+print(student)
+print(student["subjects"])
+print(student["subjects"]["math"])
