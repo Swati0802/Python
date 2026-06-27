af05600088314ca2fe0e2315d22c9f08cd121dd0
Swati0802 <25it445@bvmengineering.ac.in>
2026-06-27 14:14:24 +0530

    Create Set.py

---
+++ Practice/Set.py
@@ -0,0 +1,11 @@
+collection = {1, 2, 2, 2, 3, 4, "hello", "world"} 
+print(collection)
+print(type(collection))
+print(len(collection)) #total number of items
+
+
+collection = {}  # empty dictionary
+print(type(collection))
+
+collection = set()  # empty set
+print(type(collection))
