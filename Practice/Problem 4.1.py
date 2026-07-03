59cb6f81b8eb34a9e73724c28e2b5c76bade5eeb
Swati0802 <25it445@bvmengineering.ac.in>
2026-07-03 11:26:47 +0530

    Add script to collect and print subject scores

---
+++ Practice/Problem 4.1.py
@@ -0,0 +1,12 @@
+dict = {}
+
+x = int(input("Enter Phy :"))
+dict.update({"Phy" : x})
+
+y = int(input("Enter Chem :"))
+dict.update({"Chem" : y})
+
+z = int(input("Enter Math :"))
+dict.update({"Math" : z})
+
+print(dict)
