7e6caab2c4f4517c2e2634b50fd8b64b77d26009
Swati0802 <25it445@bvmengineering.ac.in>
2026-07-03 11:25:58 +0530

    Create Problem 4.py

---
+++ Practice/Problem 4.py
@@ -0,0 +1,12 @@
+# 1
+dict = {
+    "cat" : "a small animal",
+    "table" : ["a piece of furniture", "list of facts & figures"]
+}
+print(dict)
+
+# 2
+set = {
+  "Python", "Java", "C++", "Python", "JavaScript", "Java", "Python", "Java", "C++", "C"
+ }
+print(len(set))
