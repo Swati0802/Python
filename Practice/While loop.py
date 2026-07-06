39726668b63c133069ad5c2fd85a9e39bda09a41
Swati0802 <25it445@bvmengineering.ac.in>
2026-07-07 00:08:56 +0530

    Add examples of while loops in Python
    Added multiple while loop examples demonstrating iteration and counting.

---
+++ Practice/While loop.py
@@ -0,0 +1,28 @@
+# while True :
+#     print("Hello")
+# Loop work = Iteration
+
+count = 1   # count = Iterator
+while count <= 5 :
+    print("Hello")
+    count = count + 1
+
+# print(count)
+
+i = 1 # Initialization
+while i <= 5 :
+    print("world", i)
+    i = i + 1
+
+    print(i)
+
+# new
+i = 1
+while i <= 5 :
+    print(i)
+    i = i + 1
+
+i = 5
+while i >= 1 :
+    print(i)
+    i = i - 1
