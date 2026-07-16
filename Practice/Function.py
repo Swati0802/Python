37d3afafbc25f9ad868963cd8fa46c6157a555fb
Swati0802 <25it445@bvmengineering.ac.in>
2026-07-16 08:24:09 +0530

    Create Function.py

---
+++ Practice/Function.py
@@ -0,0 +1,24 @@
+def calc_sum(a,b):
+    sum = a + b
+    print(sum)
+    return sum
+
+calc_sum(1,2)
+
+#  New Simple
+#Function Definition
+def calc_sum(a, b): #parameters 
+    return a + b
+
+sum = calc_sum(2, 3)  #function call; arguments
+print(sum)
+
+
+#
+def print_hello():
+    print("hello")
+
+print_hello()
+
+output = print_hello()
+print(output)  #None
