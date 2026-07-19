2c621be006023a924a112348361e138a9c79e2ab
Swati0802 <25it445@bvmengineering.ac.in>
2026-07-19 17:09:46 +0530

    Create Sep,end.py

---
+++ Practice/Sep,end.py
@@ -0,0 +1,10 @@
+print("apnacollege","shradha") #sep = " "
+
+print("apnacollege")  #end = "\n"
+print("shradha")
+
+print("apnacollege", end=" ")
+print("shradha")
+
+print("apnacollege", end="$")
+print("shradha")
